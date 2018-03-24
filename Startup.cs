47eac32173eccdaa8b2e2c4550246ac1458d3f94
Mark Heath <mark.heath@gmail.com>
2018-03-24 15:10:53 +0000

    using Razor pages

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace coreapp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,7 @@ namespace coreapp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello ACI World!");
-            });
+            app.UseMvc();
         }
     }
 }
